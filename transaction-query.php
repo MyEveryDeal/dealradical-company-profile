407ca825c1933a8b4a56060141e92271c3c1214f
ajagabos007 <ajagabos007@gmail.com>
2023-04-11 02:41:35 +0100

    update transaction query

--- transaction-query.php
+++ transaction-query.php
@@ -20,41 +20,158 @@
                         <div class="tabs-box">
                             <div class="tab-btn-box">
                                 <ul class="tab-btns tab-buttons clearfix">
-                                    <li class="tab-btn active-btn text-dark" data-tab="#tab-4">Check your transaction</li>
+                                    <li class="tab-btn active-btn text-dark" data-tab="#tab-4">One Time User</li>
+                                    <li class="tab-btn text-dark" data-tab="#tab-5">Members Login</li>
                                 </ul>
                             </div>
                             <div class="tabs-content">
                                 <div class="tab active-tab" id="tab-4">
                                     <div class="content-inner">
-                                        <p class="text-dark">Leveraging NIBSS to validate lagging transactions at a click. Call us <a href="tel:2347017518376" class="text-muted">2347017518376</a></p>
+                                        <p class="text-dark">Leveraging NIBSS to validate lagging transactions at a click or call us on <a href="tel:2347017518376" >2347017518376</a></p>
                                         <form action="transaction-result.php" method="post">
-                                            <div class="form-group">
-                                                <i class="fas fa-swatchbook"></i>
-                                                <input type="text" name="transaction_number" placeholder="Transaction ref. no." required class="form-control">
+                                            <div class="row">
+                                            <div class="col-md-6 mb-3">
+                                                    <label for="transaction_date">Transaction date</label>
+                                                    <div class="input-group  ">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text" id="basic-addon1">
+                                                                <i class="fas fa-calendar"></i>
+                                                            </span>
+                                                        </div>
+                                                        <input type="date" name="transaction_date" placeholder="Transaction date" required class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6 mb-3">
+                                                    <label for="transaction_number">Transaction number</label>
+                                                    <div class="input-group  ">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text" id="basic-addon1">
+                                                                <i class="fas fa-swatchbook"></i>
+                                                            </span>
+                                                        </div>
+                                                        <input type="number" name="transaction_number" placeholder="Trnx no." required class="form-control">
+                                                    </div>
+                                                </div>                                                
                                             </div>
-                                            <div class="form-group">
-                                                <i class="fas fa-id-card"></i>
-                                                <input type="text" name="BVN" placeholder="BVN" required="" class="form-control" required>
-                                            </div>
-                                            <div class="form-group">
-                                                <i class="fas fa-piggy-bank"></i>
-                                                <div class="select-box">
-                                                    <select class="wide" name="bank" required>
-                                                        <option  display="First Bank" class="option">Select bank</option>
-                                                        <option value="First Bank" display="First Bank" class="option">First Bank</option>
-                                                        <option value="UBA" class="option">UBA</option>
-                                                        <option value="Opay" class="option">Opay</option>
-                                                        <option value="Access Bank" class="option">Access Bank</option>
-                                                        <option value="FCMB" class="option">FCMB</option>
-                                                    </select>
+                                            <div class="row">
+                                                
+                                                <div class="col-12 mb-3">
+                                                    <label for="amount">Amount (#)</label>
+                                                    <div class="input-group  ">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text" id="basic-addon1">
+                                                                <i class="fas fa-money-bill-alt"></i>
+                                                            </span>
+                                                        </div>
+                                                        <input type="number" step="0.01" name="amount" placeholder="0.00" required class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="mb-3">
+                                                        <label for="sender_account_number">Sender Bank</label>
+                                                        <div class="form-group">
+                                                            <i class="fas fa-piggy-bank"></i>
+                                                            <div class="select-box">
+                                                                <select class="wide" name="bank" required>
+                                                                    <option  display="First Bank" class="option">Select bank</option>
+                                                                    <option value="First Bank" display="First Bank" class="option">First Bank</option>
+                                                                    <option value="UBA" class="option">UBA</option>
+                                                                    <option value="Opay" class="option">Opay</option>
+                                                                    <option value="Access Bank" class="option">Access Bank</option>
+                                                                    <option value="FCMB" class="option">FCMB</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="sender_account_number">Sender Account Number</label>
+                                                        <div class="input-group  ">
+                                                            <div class="input-group-prepend">
+                                                                <span class="input-group-text" id="basic-addon1">
+                                                                    <i class="fas fa-credit-card"></i>
+                                                                </span>
+                                                            </div>
+                                                            <input type="number" name="sender_account_number" placeholder="account number" required class="form-control">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                
+                                                <div class="col-md-6">
+
+                                                    <div class="mb-3">
+                                                        <label for="sender_account_number">Receiver Bank</label>
+                                                        <div class="form-group">
+                                                            <i class="fas fa-piggy-bank"></i>
+                                                            <div class="select-box">
+                                                                <select class="wide" name="bank" required>
+                                                                    <option  display="First Bank" class="option">Select bank</option>
+                                                                    <option value="First Bank" display="First Bank" class="option">First Bank</option>
+                                                                    <option value="UBA" class="option">UBA</option>
+                                                                    <option value="Opay" class="option">Opay</option>
+                                                                    <option value="Access Bank" class="option">Access Bank</option>
+                                                                    <option value="FCMB" class="option">FCMB</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="receiver_account_number">Receiver Account Number</label>
+                                                        <div class="input-group  ">
+                                                            <div class="input-group-prepend">
+                                                                <span class="input-group-text" id="basic-addon1">
+                                                                    <i class="fas fa-credit-card"></i>
+                                                                </span>
+                                                            </div>
+                                                            <input type="number" name="receiver_account_number" placeholder="account number" required class="form-control">
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
+                                            
+                                            
+                                            
+                                            
                                             <div class="form-group message-btn">
                                                 <button type="submit" onclick="submit()" class="theme-btn style-one w-full">Submit</button>
                                             </div>
                                         </form>
                                     </div>
                                 </div>
+                                <div class="tab" id="tab-5">
+                                    <p class="text-dark">Gain member's full access to NIBSS transaction tracking and validation</p>
+                                   
+                                    <form action="transaction-result.php" method="post">
+                                            <div class="row">
+                                            <div class="col-md-12 mb-3">
+                                                    <label for="email">Email</label>
+                                                    <div class="input-group  ">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text" id="basic-addon1">
+                                                                <i class="fas fa-envelope"></i>
+                                                            </span>
+                                                        </div>
+                                                        <input type="email" name="email" placeholder="" required class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12 mb-3">
+                                                    <label for="transaction_number">Password</label>
+                                                    <div class="input-group  ">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text" id="basic-addon1">
+                                                                <i class="fas fa-key"></i>
+                                                            </span>
+                                                        </div>
+                                                        <input type="password" name="password" placeholder="" required class="form-control">
+                                                    </div>
+                                                </div>                                                
+                                            </div>
+                                           
+                                            <div class="form-group message-btn">
+                                                <button type="submit" onclick="submit()" class="theme-btn style-one w-full">Login</button>
+                                            </div>
+                                        </form>
+                                </div>
+
                             </div>
                         </div>
                     </div>
